19bc04574a8dc3410b4b6e1d8123c629b1133b0b
mariv <nbt1032@gmail.com>
2021-09-27 19:02:53 -0400

    sep27-prime

--- EFGrid/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/CustomAdaptorComponent.razor.g.cs
+++ EFGrid/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/CustomAdaptorComponent.razor.g.cs
@@ -13,126 +13,126 @@ namespace EFGrid.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 1 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 2 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 3 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 4 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 5 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 6 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 7 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 8 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using EFGrid;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
+#line 9 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\_Imports.razor"
 using EFGrid.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 1 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using Syncfusion.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 2 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using Syncfusion.Blazor.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 3 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 4 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using static EFGrid.Pages.Index;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 5 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using Microsoft.Data.SqlClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 6 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using System.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 7 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 8 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using System.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 9 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
 using Microsoft.AspNetCore.Hosting;
 
 #line default
@@ -146,7 +146,7 @@ using Microsoft.AspNetCore.Hosting;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 19 "C:\Users\marc\Source\Repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
+#line 19 "C:\Users\marc\source\repos\blazor-grid-sqldatabinding\EFGrid\Shared\CustomAdaptorComponent.razor"
        
     [Parameter]
     [JsonIgnore]
